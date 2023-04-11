3d1b4af71dff16b423f9b35481e9b930955a8f18
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-04-11 13:11:39 +0530

    added form data and admin page routes

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.post('/check-bday', (req, res) => {
+    console.log(req.body)
+})
+
+app.get('/admin/add-data', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(5000, () => {
